e7eeb5af597233d5ac2db9bac8eeca0ea2931381
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2023-11-19 14:38:29 -0300

    alteração do index

--- public/index.php
+++ public/index.php
@@ -7,9 +7,7 @@ use core\Controller;
 try {
     $controller = new Controller;
     $controller = $controller->load();
+    dd($controller);
 } catch (Exception $e) {
     dd($e->getMessage());
 }
-
-
-dd($controller);
