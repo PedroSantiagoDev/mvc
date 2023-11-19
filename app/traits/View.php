09ad739bb56d5a082368fbbd832f4a83ed287f32
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2023-11-19 19:50:15 -0300

    criação da view e metodos

---
+++ app/traits/View.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\traits;
+
+use core\Twig;
+
+trait View
+{
+
+    private function twig()
+    {
+        $twig = new Twig;
+        $loadTwig =  $twig->loadTwig();
+
+        $twig->loadExtensions();
+
+        return $loadTwig;
+    }
+
+    public function view($data, $view,)
+    {
+        $template = $this->twig()->load(str_replace(".", "/", $view) . ".html");
+
+        return $template->desplay($data);
+    }
+}
