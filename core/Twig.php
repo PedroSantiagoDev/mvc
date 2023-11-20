6b98aa319d858895a4d429c0eb862310028a94f9
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2023-11-20 15:09:08 -0300

    criação e configurações do twig

--- core/Twig.php
+++ core/Twig.php
@@ -2,6 +2,8 @@
 
 namespace core;
 
+use Closure;
+
 class Twig
 {
     private $twig;
@@ -9,7 +11,7 @@ class Twig
 
     public function loadTwig()
     {
-        $this->twig = new \Twig_Environment($this->loadViews(), [
+        $this->twig = new \Twig\Environment($this->loadViews(), [
             "debug" => true,
             //"cache" => "/cache",
             "auto-reload" => true
@@ -20,11 +22,25 @@ class Twig
 
     private function loadViews()
     {
-        return new \Twig_Loader_Filesystem('../app/views');
+        return new \Twig\Loader\FilesystemLoader('../app/views');
     }
 
     public function loadExtensions()
     {
-        return $this->twig->addExtension(new \Twig_Extension_Extension_text());
+        return $this->twig->addExtension(new \Twig\Extension\StringLoaderExtension());
+    }
+
+    private function functionsToView($name, Closure $callbak)
+    {
+        return new \Twig\TwigFunction($name, $callbak);
+    }
+
+    public function loadFunctions()
+    {
+        require "../app/functions/twig.php";
+
+        foreach ($this->functions as $key => $value) {
+            $this->twig->addFunction($this->functions[$key]);
+        }
     }
 }
